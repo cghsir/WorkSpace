bce82b3e9296a9eab23f832bb05f2f96774ebb5e
cghsir <cghsir@gmail.com>
2017-07-06 20:02:30 +0800

    spring boot项目运行主入口

---
+++ rigger/src/main/java/com/cghsir/Application.java
@@ -0,0 +1,15 @@
+package com.cghsir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目运行主入口
+ * Created by cghsir on 2017/7/6.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
