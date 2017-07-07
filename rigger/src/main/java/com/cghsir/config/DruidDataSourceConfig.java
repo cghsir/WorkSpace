786b761b60e062c857b30e0ae1c0c5a19b71d602
cghsir <cghsir@gmail.com>
2017-07-07 09:26:07 +0800

    druid 数据源配置类

---
+++ rigger/src/main/java/com/cghsir/config/DruidDataSourceConfig.java
@@ -0,0 +1,54 @@
+package com.cghsir.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * druid数据源配置类
+ * Created by cghsir on 2017/7/6.
+ */
+@Configuration
+@EnableTransactionManagement
+public class DruidDataSourceConfig  implements EnvironmentAware {
+
+    private RelaxedPropertyResolver propertyResolver;
+
+    public void setEnvironment(Environment env) {
+        this.propertyResolver = new RelaxedPropertyResolver(env, "spring.datasource.");
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        System.out.println("注入druid！！！");
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(propertyResolver.getProperty("url"));
+        datasource.setDriverClassName(propertyResolver.getProperty("driver-class-name"));
+        datasource.setUsername(propertyResolver.getProperty("username"));
+        datasource.setPassword(propertyResolver.getProperty("password"));
+        System.out.println(propertyResolver.getProperty("driver-class-name"));
+        System.out.println(propertyResolver.getProperty("username"));
+        System.out.println(propertyResolver.getProperty("password"));
+        System.out.println(propertyResolver.getProperty("initial-size"));
+        datasource.setInitialSize(Integer.valueOf(propertyResolver.getProperty("druid.initial-size")));
+        datasource.setMinIdle(Integer.valueOf(propertyResolver.getProperty("druid.min-idle")));
+        datasource.setMaxWait(Long.valueOf(propertyResolver.getProperty("druid.max-wait")));
+        datasource.setMaxActive(Integer.valueOf(propertyResolver.getProperty("druid.max-active")));
+        datasource.setMinEvictableIdleTimeMillis(Long.valueOf(propertyResolver.getProperty("druid.min-evictable-idle-time-millis")));
+        try {
+            datasource.setFilters("stat,wall");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return datasource;
+    }
+}
