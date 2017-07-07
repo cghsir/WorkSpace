39fd00cd8d9367edb2d0e2002d72f9bafc79c320
cghsir <cghsir@gmail.com>
2017-07-07 09:27:25 +0800

    添加mybatis mapper扫描，servlet包扫描

--- rigger/src/main/java/com/cghsir/Application.java
+++ rigger/src/main/java/com/cghsir/Application.java
@@ -1,13 +1,17 @@
 package com.cghsir;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * 项目运行主入口
  * Created by cghsir on 2017/7/6.
  */
+@MapperScan("com.cghsir.model.mapper")//扫描mybatis mpper文件
 @SpringBootApplication
+@ServletComponentScan//扫描主类下的Servlet
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
