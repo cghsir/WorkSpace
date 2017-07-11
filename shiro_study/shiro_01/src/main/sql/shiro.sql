0da445cfae44abee66c6266e2c8b975d687207d0
cghsir <cghsir@gmail.com>
2017-07-11 13:53:41 +0800

    shiro_01_自定义jdbc druid方式的realm

---
+++ shiro_study/shiro_01/src/main/sql/shiro.sql
@@ -0,0 +1,61 @@
+--引用开涛的 sql文件 https://github.com/zhangkaitao/shiro-example/blob/master/shiro-example-chapter2/src/sql/shiro.sql
+drop database if exists shiro;
+create database shiro;
+use shiro;
+
+create table users (
+  id bigint auto_increment,
+  username varchar(100),
+  password varchar(100),
+  password_salt varchar(100),
+  constraint pk_users primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_users_username on users(username);
+
+create table user_roles(
+  id bigint auto_increment,
+  username varchar(100),
+  role_name varchar(100),
+  constraint pk_user_roles primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_user_roles on user_roles(username, role_name);
+
+create table roles_permissions(
+  id bigint auto_increment,
+  role_name varchar(100),
+  permission varchar(100),
+  constraint pk_roles_permissions primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_roles_permissions on roles_permissions(role_name, permission);
+
+insert into users(username,password)values('zhang','123');
+drop database if exists shiro;
+create database shiro;
+use shiro;
+
+create table users (
+  id bigint auto_increment,
+  username varchar(100),
+  password varchar(100),
+  password_salt varchar(100),
+  constraint pk_users primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_users_username on users(username);
+
+create table user_roles(
+  id bigint auto_increment,
+  username varchar(100),
+  role_name varchar(100),
+  constraint pk_user_roles primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_user_roles on user_roles(username, role_name);
+
+create table roles_permissions(
+  id bigint auto_increment,
+  role_name varchar(100),
+  permission varchar(100),
+  constraint pk_roles_permissions primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create unique index idx_roles_permissions on roles_permissions(role_name, permission);
+
+insert into users(username,password)values('zhang','123');
